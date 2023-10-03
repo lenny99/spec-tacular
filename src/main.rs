38314271d12a01bd090cb723807e2f3daca80ac7
Lennart Grosse <lennartgrosse@hotmail.de>
2023-10-03 13:25:30 +0200

    parse newtype with format

--- src/main.rs
+++ src/main.rs
@@ -67,16 +67,25 @@ mod parser {
                 Rule::TypeDef => {
                     let mut inners = inner.into_inner().into_iter();
                     let primitive = ApiScript::primitive(inners.next().unwrap());
-                    let format = inners
-                        .next()
-                        .map(|p| p.as_str().to_owned())
-                        .or(Option::None);
+                    let format = inners.next().map(ApiScript::format);
                     SchemaDefinition::NewType { primitive, format }
                 }
                 Rule::Fields => todo!(),
                 _ => unimplemented!(),
             };
             return Ok(schema_definition);
+        }
+
+        fn format(format: Node) -> Format {
+            let format_string = format.as_str();
+            match format_string {
+                "date" => Format::Date,
+                "date-time" => Format::DateTime,
+                "password" => Format::Password,
+                "byte" => Format::Byte,
+                "binary" => Format::Binary,
+                _ => Format::Custom(format_string.to_owned()),
+            }
         }
 
         fn primitive(primitive: Node) -> Primitive {
@@ -100,7 +109,7 @@ mod parser {
     pub enum SchemaDefinition {
         NewType {
             primitive: Primitive,
-            format: Option<String>,
+            format: Option<Format>,
         },
         Array {
             schemaRef: Rc<Schema>,
@@ -111,29 +120,36 @@ mod parser {
     }
 
     #[derive(Debug)]
-
     pub struct Field {
         pub name: String,
         pub definition: SchemaDefinition,
     }
 
     #[derive(Debug)]
-
     pub enum Primitive {
         String,
         Number,
         Integer,
         Boolean,
     }
+
+    #[derive(Debug, Clone)]
+    pub enum Format {
+        Date,
+        DateTime,
+        Password,
+        Byte,
+        Binary,
+        Custom(String),
+    }
 }
 
 mod generator {
+    use crate::parser::{ApiScript, Format, Primitive, Schema, SchemaDefinition};
     use indexmap::IndexMap;
-    use openapiv3::{Components, Info, OpenAPI, Paths, Server};
+    use openapiv3;
 
-    use crate::parser::ApiScript;
-
-    pub(crate) fn generate(api_script: &ApiScript) -> OpenAPI {
+    pub(crate) fn generate(api_script: &ApiScript) -> openapiv3::OpenAPI {
         return api_script.generate();
     }
 
@@ -141,19 +157,25 @@ mod generator {
         fn generate(&self) -> T;
     }
 
-    impl Generate<OpenAPI> for ApiScript {
-        fn generate(&self) -> OpenAPI {
-            return OpenAPI {
-                components: Some(Components {
-                    schemas: IndexMap::new(),
+    impl Generate<openapiv3::OpenAPI> for ApiScript {
+        fn generate(&self) -> openapiv3::OpenAPI {
+            return openapiv3::OpenAPI {
+                components: Some(openapiv3::Components {
+                    schemas: self.schemas.iter().map(Schema::generate).fold(
+                        IndexMap::new(),
+                        |mut map, (identificator, schema)| {
+                            map.insert(identificator, openapiv3::ReferenceOr::Item(schema));
+                            return map;
+                        },
+                    ),
                     ..Default::default()
                 }),
                 openapi: String::from("3.1.0"),
-                info: Info {
+                info: openapiv3::Info {
                     ..Default::default()
                 },
                 servers: vec![],
-                paths: Paths {
+                paths: openapiv3::Paths {
                     ..Default::default()
                 },
                 security: None,
@@ -161,6 +183,63 @@ mod generator {
                 external_docs: None,
                 extensions: IndexMap::new(),
             };
+        }
+    }
+
+    impl Schema {
+        fn generate(&self) -> (String, openapiv3::Schema) {
+            let schema = match &self.schema_definition {
+                SchemaDefinition::NewType { primitive, format } => openapiv3::Schema {
+                    schema_kind: primitive.as_schema_type(format.clone()), // ?
+                    schema_data: openapiv3::SchemaData::default(),
+                },
+                _ => todo!(),
+            };
+            return (self.identificator.to_owned(), schema);
+        }
+    }
+
+    impl Primitive {
+        fn as_schema_type(&self, format: Option<Format>) -> openapiv3::SchemaKind {
+            let openapi_format: openapiv3::VariantOrUnknownOrEmpty<openapiv3::StringFormat> =
+                format.map_or(openapiv3::VariantOrUnknownOrEmpty::Empty, Format::as_format);
+            let openapi_type = match self {
+                Primitive::String => openapiv3::Type::String(openapiv3::StringType {
+                    format: openapi_format,
+                    ..Default::default()
+                }),
+                Primitive::Number => openapiv3::Type::Number(openapiv3::NumberType::default()),
+                Primitive::Integer => openapiv3::Type::Integer(openapiv3::IntegerType::default()),
+                Primitive::Boolean => openapiv3::Type::Boolean {},
+            };
+            return openapiv3::SchemaKind::Type(openapi_type);
+        }
+    }
+
+    impl Format {
+        fn as_format(self) -> openapiv3::VariantOrUnknownOrEmpty<openapiv3::StringFormat> {
+            match self {
+                Format::Date => {
+                    openapiv3::VariantOrUnknownOrEmpty::Item(openapiv3::StringFormat::Date)
+                }
+                Format::DateTime => todo!(),
+                Format::Password => todo!(),
+                Format::Byte => todo!(),
+                Format::Binary => todo!(),
+                Format::Custom(_) => todo!(),
+            }
+        }
+    }
+
+    impl Into<openapiv3::SchemaKind> for &Primitive {
+        fn into(self) -> openapiv3::SchemaKind {
+            let openapi_type = match self {
+                Primitive::String => openapiv3::Type::String(openapiv3::StringType::default()),
+                Primitive::Number => openapiv3::Type::Number(openapiv3::NumberType::default()),
+                Primitive::Integer => openapiv3::Type::Integer(openapiv3::IntegerType::default()),
+                Primitive::Boolean => openapiv3::Type::Boolean {},
+            };
+            return openapiv3::SchemaKind::Type(openapi_type);
         }
     }
 }
